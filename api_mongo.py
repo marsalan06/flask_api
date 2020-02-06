6a2893a08b7c479a96e68f709cb3db74f94ec26f
marsalan06 <arsalan.9798@gmail.com>
2020-02-06 21:16:50 +0500

    PUT AND DELETE METHOD ADDED

--- api_mongo.py
+++ api_mongo.py
@@ -1,10 +1,10 @@
-from flask import Flask, request,jsonify
+from flask import Flask, request,jsonify,redirect,url_for
 from flask_pymongo import PyMongo
 import json
 
 app=Flask(__name__)
 app.config['MONGO_DBNAME']='udemy_collect' #project name under cluster0
-app.config['MONGO_URI']="mongodb+srv://test:****@cluster0-yro7g.mongodb.net/api_flask?retryWrites=true&w=majority"
+app.config['MONGO_URI']="mongodb+srv://test:test@cluster0-yro7g.mongodb.net/api_flask?retryWrites=true&w=majority"
 #add database name above after .net/
 mongo=PyMongo(app)
 
@@ -34,7 +34,42 @@ def add_data():
     entery_id=data.insert({'name':name,'age':age})
     get_data=data.find_one({"_id":entery_id})
     output={'name':get_data['name'],'age':get_data['age']}
-    return jsonify({'output':output})
+    return jsonify({'result':output})
+
+@app.route("/data/<name>",methods=['PUT'])
+def edit_data(name):
+    data=mongo.db.user_data
+    q=data.find_one({"name":name})
+    if q:
+        old={"name":q['name'],"age":q['age']}
+
+        if  'name' in request.json:
+            n_name=request.json['name']
+        else:
+            n_name=q['name']
+        if 'age' in request.json:
+            n_age=request.json['age']
+        else:
+            n_age=q['age']
+        if n_name or n_age:
+            new={"$set":{"name":n_name,"age":n_age}}
+            entery_id=data.update_one(old,new)
+            return redirect(url_for("all_data"))
+    else:
+        output="No feild present for " + str(name)
+        return jsonify({"result":output})
+
+@app.route("/data/<string:name>",methods=['DELETE'])
+def delete_data(name):
+    data=mongo.db.user_data
+    q=data.find_one({"name":name})
+    if q:
+        data.delete_one(q)
+        return redirect(url_for("all_data"))
+    else:
+        output="No feild present for " + str(name)
+        return jsonify({"result":output})
+
 
 
 app.run(host='192.168.1.104',debug=True)
